902d217220ba57bf6716f9f88a13faeaa61bbdba
hasan tok <hasantok2955@hotmail.com>
2021-01-16 23:46:16 +0300

    16 Ocak

---
+++ Kampintro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kampintro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string kategoriEtiketi = "Kategori";
+			int ogrenciSayisi = 32000;
+			double faizOrani = 1.45;
+			bool sistemeGirisYapmisMi = true;
+			double dolarBugun
+
+			if (sistemeGirisYapmisMi == true)
+			{
+				Console.WriteLine("kullanıcı ayarları butonu");
+			}
+			else
+			{
+				Console.WriteLine("giriş yap butonu");
+			}
+		}
+	}
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 88;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun x in urunler)
+            {
+                Console.WriteLine(x.Adi);
+                Console.WriteLine(x.Fiyati);
+                Console.WriteLine(x.Aciklama);
+                Console.WriteLine("-----------");
+
+            }
+
+            Console.WriteLine("-------Metotlar-----");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12);
+        }
+    }
+}
